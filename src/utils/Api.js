75077a2e69330474c08978f33367d349810edba2
Sanni Kumar <aryansingh7371@gmail.com>
2025-06-20 16:42:38 +0530

    hello

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,5 @@
-const token = import.meta.env.VITE_TOKEN;
+// const token = import.meta.env.VITE_TOKEN;
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZTRkNDA3NjU4YWI3MjkyMDlmNmQ3ZTM3YzdhZjAyZSIsIm5iZiI6MTcxMTYzMDU1MS41OTUsInN1YiI6IjY2MDU2OGQ3ZWNhZWY1MDE2MWFmOWI2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nReFhKbkU5ojRqqOyo9czsmUZQRD3UXvVB9qGgQJUq4'
 const options = {
     method: 'GET',
     headers: {
